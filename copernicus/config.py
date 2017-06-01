a7eaa661bc87c9d29a54fb3d88df116b5ff2fa79
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-01 13:53:28 +0200

    use conda env path for esmval_root

--- copernicus/config.py
+++ copernicus/config.py
@@ -1,3 +1,6 @@
+import sys
+import os
+
 from pywps import configuration
 
 import logging
@@ -9,4 +12,8 @@ def archive_root():
 
 
 def esmval_root():
-    return configuration.get_config_value("extra", "esmval_root")
+    root = configuration.get_config_value("extra", "esmval_root")
+    if not root:
+        # guess conda env path
+        root = os.path.join(os.path.dirname(sys.executable), '..', 'opt', 'esmvaltool')
+    return root
