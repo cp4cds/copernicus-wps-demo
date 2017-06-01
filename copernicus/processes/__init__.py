44ee3e4805a98889a494ccfec918a09ca0beb03b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-01 15:42:40 +0200

    removed hello process

--- copernicus/processes/__init__.py
+++ copernicus/processes/__init__.py
@@ -1,10 +1,8 @@
-from .wps_say_hello import SayHello
 from .wps_mydiag import MyDiag
 from .wps_overview import Overview
 from .wps_timeseries_plot import TimeSeriesPlot
 
 processes = [
-    SayHello(),
     MyDiag(),
     Overview(),
     TimeSeriesPlot(),
