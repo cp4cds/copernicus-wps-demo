38b048fb0a497fba7c5e3893477377eeff64ce42
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-01 17:36:05 +0200

    added generic timeseries plot

--- copernicus/processes/__init__.py
+++ copernicus/processes/__init__.py
@@ -1,9 +1,11 @@
 from .wps_mydiag import MyDiag
 from .wps_overview import Overview
 from .wps_timeseries_plot import TimeSeriesPlot
+from .wps_timeseries_plot_generic import GenericTimeSeriesPlot
 
 processes = [
     MyDiag(),
     Overview(),
     TimeSeriesPlot(),
+    GenericTimeSeriesPlot(),
 ]
