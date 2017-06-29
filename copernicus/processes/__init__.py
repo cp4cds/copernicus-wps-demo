b083faf8c045315afca5a6a778f0bad5c607b2f0
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-29 12:48:15 +0200

    disabled generic contour plot

--- copernicus/processes/__init__.py
+++ copernicus/processes/__init__.py
@@ -2,12 +2,12 @@ from .wps_mydiag import MyDiag
 from .wps_overview import Overview
 from .wps_timeseries_plot import TimeSeriesPlot
 from .wps_timeseries_plot_generic import GenericTimeSeriesPlot
-from .wps_contour_plot import ContourPlot
+# from .wps_contour_plot import ContourPlot
 
 processes = [
     MyDiag(),
     Overview(),
     TimeSeriesPlot(),
     GenericTimeSeriesPlot(),
-    ContourPlot(),
+    # ContourPlot(),
 ]
