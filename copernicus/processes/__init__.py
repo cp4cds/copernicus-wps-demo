37255b69f11085554923ed487df1246a9a661a5c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-29 14:06:07 +0200

    added initial taylor plot

--- copernicus/processes/__init__.py
+++ copernicus/processes/__init__.py
@@ -1,5 +1,6 @@
 from .wps_mydiag import MyDiag
 from .wps_overview import Overview
+from .wps_taylor import Taylor
 from .wps_timeseries_plot import TimeSeriesPlot
 from .wps_timeseries_plot_generic import GenericTimeSeriesPlot
 # from .wps_contour_plot import ContourPlot
@@ -7,6 +8,7 @@ from .wps_timeseries_plot_generic import GenericTimeSeriesPlot
 processes = [
     MyDiag(),
     Overview(),
+    Taylor(),
     TimeSeriesPlot(),
     GenericTimeSeriesPlot(),
     # ContourPlot(),
