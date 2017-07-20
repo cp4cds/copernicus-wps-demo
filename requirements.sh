35289dfa3390c70a9f1cb86e16ab64098ee60836
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-07-20 14:28:58 +0200

    added drmaa slurm debian packages

--- requirements.sh
+++ requirements.sh
@@ -2,7 +2,8 @@
 
 if [ -f /etc/debian_version ] ; then
     echo "Installing Debian/Ubuntu packages ..."
-    #sudo apt-get -y install wget
+    # drmaa with slurm extension
+    sudo apt-get install -y build-essential slurm-drmaa1 slurm-drmaa-dev slurm-llnl
 elif [ -f /etc/redhat-release ] ; then
     echo "Installing RedHat/CentOS packages ..."
     #sudo yum -y install wget
