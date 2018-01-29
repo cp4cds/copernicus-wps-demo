f95e4064eb2b6eb1105d00fe8617ab696990ac16
Carsten Ehbrecht <ehbrecht@dkrz.de>
2018-01-29 15:40:51 +0100

    disabled currently not used processes

--- copernicus/processes/__init__.py
+++ copernicus/processes/__init__.py
@@ -1,17 +1,17 @@
 from .wps_mydiag import MyDiag
-from .wps_overview import Overview
-from .wps_cloud_taylor import CloudTaylor
-from .wps_portrait import Portrait
-from .wps_timeseries_plot import TimeSeriesPlot
-from .wps_timeseries_plot_generic import GenericTimeSeriesPlot
+# from .wps_overview import Overview
+# from .wps_cloud_taylor import CloudTaylor
+# from .wps_portrait import Portrait
+# from .wps_timeseries_plot import TimeSeriesPlot
+# from .wps_timeseries_plot_generic import GenericTimeSeriesPlot
 # from .wps_contour_plot import ContourPlot
 
 processes = [
     MyDiag(),
-    Overview(),
-    CloudTaylor(),
-    Portrait(),
-    TimeSeriesPlot(),
-    GenericTimeSeriesPlot(),
+    # Overview(),
+    # CloudTaylor(),
+    # Portrait(),
+    # TimeSeriesPlot(),
+    # GenericTimeSeriesPlot(),
     # ContourPlot(),
 ]
