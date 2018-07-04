6476d06f971b06458671995d05971880ba896b11
Pingu Carsti <ehbrecht@dkrz.de>
2018-07-04 17:39:25 +0200

    added metadata to pydemo

--- copernicus/processes/wps_pydemo.py
+++ copernicus/processes/wps_pydemo.py
@@ -7,6 +7,7 @@ from pywps import Format, FORMATS
 from pywps.app.Common import Metadata
 
 from copernicus import runner
+from copernicus import util
 
 import logging
 LOGGER = logging.getLogger("PYWPS")
@@ -62,7 +63,14 @@ class PyDemo(Process):
              " project=CMIP5, experiment=historical, ensemble=r1i1p1, variable=ta, model=MPI-ESM-LR, time_frequency=mon",  # noqa
             metadata=[
                 Metadata('ESMValTool', 'http://www.esmvaltool.org/'),
-                Metadata('ESGF Testdata', 'https://esgf1.dkrz.de/thredds/catalog/esgcet/7/cmip5.output1.MPI-M.MPI-ESM-LR.historical.mon.atmos.Amon.r1i1p1.v20120315.html?dataset=cmip5.output1.MPI-M.MPI-ESM-LR.historical.mon.atmos.Amon.r1i1p1.v20120315.ta_Amon_MPI-ESM-LR_historical_r1i1p1_199001-199912.nc'),  # noqa
+                Metadata('Documentation',
+                         'https://copernicus-wps-demo.readthedocs.io/en/latest/processes.html#pydemo',
+                         role=util.WPS_ROLE_DOC),
+                Metadata('Media',
+                         util.diagdata_url() + '/pydemo/pydemo_thumbnail.png',
+                         role=util.WPS_ROLE_MEDIA),
+                Metadata('ESGF Testdata',
+                         'https://esgf1.dkrz.de/thredds/catalog/esgcet/7/cmip5.output1.MPI-M.MPI-ESM-LR.historical.mon.atmos.Amon.r1i1p1.v20120315.html?dataset=cmip5.output1.MPI-M.MPI-ESM-LR.historical.mon.atmos.Amon.r1i1p1.v20120315.ta_Amon_MPI-ESM-LR_historical_r1i1p1_199001-199912.nc'),  # noqa
             ],
             inputs=inputs,
             outputs=outputs,
